85bab9b89bfbcf16afc140bb6fa08a5a906c35c3
Benjamin Robertson <ben.robertson@puppet.com>
2022-03-15 11:29:32 +1100

    Updates

--- pe-backup-script.sh
+++ pe-backup-script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+#
+# Script used to backup Puppet Enterprise. Use within crontab.
+#
+# By default will retain backups for 28 days and use the default location for PE backups.
+# Verify you have enough space and confirm backup size before using.
+#
 if [[ "$1" == "-h" ]]; then
   echo "PE backup wrapper script"
   echo "------------------------------"
@@ -13,12 +18,36 @@ if [[ "$1" == "-h" ]]; then
   exit
 fi
 
+timestamp=`date +%Y-%m-%d_%H.%M.%S`
+
+# set retain varible
+if [ -z $2 ]; then
+  echo "Set default 28 days"
+  retain=28
+else
+  retain=$2
+fi
+
+# clean up old backup fuction
+cleanup_fn () {
+    echo "Retaining backups for $2"
+    echo "Backup location at $1"
+    # clean up pe_backups
+    /bin/find $1 -name "pe_backup*" -mtime $2 -exec rm {} \;
+    # clean up secrets backup
+    /bin/find $1 -name "secrets-*" -mtime $2 -exec rm {} \;
+}
+
+# perform backup
 if [ -z $1 ]; then
-  echo "Using default backup location /var/puppetlabs/backup"
+  echo "Using default backup location /var/puppetlabs/backups"
   /opt/puppetlabs/bin/puppet-backup create
-  #tar -cf 
+  tar -cf /var/puppetlabs/backups/secrets-$timestamp.tar /etc/puppetlabs/orchestration-services/conf.d/secrets/
+  # clean up old backups
+  cleanup_fn "/var/puppetlabs/backups" $2
 else
   echo "Using $1 as backup location"
   /opt/puppetlabs/bin/puppet-backup create $1
+  tar -cf $1/secrets-$timestamp.tar /etc/puppetlabs/orchestration-services/conf.d/secrets/
+  cleanup_fn $1 $2
 fi
-
